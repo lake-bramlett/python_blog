653630e3e8ba59c35397fb03ee64bd8cd6649741
Lake Bramlett <lake.bramlett@gmail.com>
2020-04-03 15:38:53 -0700

    add pagination and password reset

--- message_board/blog/urls.py
+++ message_board/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
